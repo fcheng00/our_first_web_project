faac71169e9bd03741ad54ee28b7e8c0e8f7980a
fengcheng0126 <fengcheng0126@gmail.com>
2020-08-19 16:28:50 -0500

    psodifuhj[awoif

--- andrew/learningUse/someStuff.js
+++ andrew/learningUse/someStuff.js
@@ -165,3 +165,127 @@ function continueSample() {
         console.log(JSON.stringify(item));
     }
 }
+
+function mathSample() {
+    let price = 200;
+    let result = 0;
+    
+    //Addition (+)
+    result = price + 100;
+    console.log('price + 100 = ' + result.toString());
+
+    //Subtraction (-)
+    result = price - 10;
+    console.log('price - 10 = ' + result.toString());
+
+    //Multiplication (*)
+    result = price * 2;
+    console.log('price * 2 = ' + result.toString());
+
+    //Division (/)
+    result = price / 2;
+    console.log('price / 2 = ' + result.toString());
+
+    //Exponentiation (**)
+    result = price ** 2;
+    console.log('price ** 2 = ' + result.toString());
+    
+    //Modulus (%)
+    result = price % 3;
+    console.log('price % 3 =' + result.toString());
+    
+    //Increment (++)
+    result = price ++;
+    console.log('result = price++ = ' + result.toString());
+    console.log('price = ' + price.toString());
+    result = ++price;
+    console.log('result = ++price = ' + result.toString());
+
+    //Decrement (--)
+    result = price --
+    console.log('result = price-- = ' + result.toString());
+    console.log('price = ' + price.toString());
+    result = --price;
+    console.log('result = --price = ' + result.toString());
+}
+
+function plusSignSample() {
+    let price = 200;
+    let stringValue = '100';
+    let result = 0;
+
+    console.log('price = ' + price.toString());
+
+    result = price + stringValue;
+    console.log('result (number + stringValue) = ' + result.toString());
+
+    result = price + (+stringValue);
+    console.log('result (number + (+stringValue)) = ' + result.toString());
+}
+
+function assignmentSample() {
+    let price = 200;
+
+    console.log('price = ' + price.toString());
+
+    //Addition (+=)
+    price += 100;
+    console.log('price += 100 = ' + price.toString());
+
+    //Subtraction (-=)
+    price -= 100;
+    console.log('price -= 100 = ' + price.toString());
+    
+    //Multiplication (*=)
+    price *= 2;
+    console.log('price *= 2 = ' + price.toString());
+
+    //Multiplication (*=)
+    price /= 2;
+    console.log('price /= 2 = ' + price.toString());
+
+    //Exponentiation (**=)
+    price **= 2;
+    console.log('price **= 2 = ' + price.toString());
+
+    //Modulus (%=)
+    price %= 3;
+    console.log('price %= 3 = ' + price.toString());
+}
+
+function comparisonSample() {
+    let price = 200;
+    let result = 0;
+
+    console.log('price = ' + price.toString());
+    console.log('');
+
+    //Equal to (==)
+    console.log('price == price = ' + (price == price).toString());
+
+    //Equal to (==)
+    result = price.toString();
+    console.log('price(number) == result(string) = ' + (price == result).toString());
+
+    //Equal to/type (===)
+    result = price;
+    console.log('price(number) === result(string) = ' + (price === result).toString());
+
+    //Equal to/type (===)
+    result = price.toString();
+    console.log('price(number) === result(string) = ' + (price === result).toString());
+    console.log('');
+
+    //Not equal to (!=)
+    result = 100;
+    console.log('result = ' + (price === result).toString());
+    console.log('price(number) != result(string) = ' + (price != result).toString());
+
+    //Not equal to/Not equal type (!==)
+    result = price.toString();
+    console.log('price(number) !== result(string) = ' + (price !== result).toString());
+    console.log('');
+
+    //Greater than (>)
+    console.log('price > 10 = ' + (price > 10).toString());
+}
