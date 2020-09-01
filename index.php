8966cb3bf2e5fbcfbdaed4779459f9da3e7edf40
fengcheng0126 <fengcheng0126@gmail.com>
2020-09-01 15:56:57 -0500

    decks to heroku

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php header('Location: /andrew/COFFEEstuff/index.html'); ?>
+
+<?php header('Location: /andrew/HTML5/Decks/index.html'); ?>
+<?php header('Location: /andrew/HTML5/Decks/A1dicks.html'); ?>
+<?php header('Location: /andrew/HTML5/Decks/A2dekcs.html'); ?>
+<?php header('Location: /andrew/HTML5/Decks/A3+4decks.html'); ?>
+<?php header('Location: /andrew/HTML5/Decks/A5+6+7decks.html'); ?>
