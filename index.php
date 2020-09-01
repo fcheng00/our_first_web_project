77b9647f1754eb2256d09d3a7d2325530074484d
fengcheng0126 <fengcheng0126@gmail.com>
2020-09-01 16:16:03 -0500

    fixe the bug

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 <?php header('Location: /andrew/COFFEEstuff/index.html'); ?>
-
-<?php header('Location: /andrew/HTML5/Decks/index.html'); ?>
-<?php header('Location: /andrew/HTML5/Decks/A1dicks.html'); ?>
-<?php header('Location: /andrew/HTML5/Decks/A2dekcs.html'); ?>
-<?php header('Location: /andrew/HTML5/Decks/A3+4decks.html'); ?>
-<?php header('Location: /andrew/HTML5/Decks/A5+6+7decks.html'); ?>
